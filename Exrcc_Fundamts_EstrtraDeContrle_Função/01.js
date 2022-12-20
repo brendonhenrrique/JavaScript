beb1cd163b40add332785efcaeba143acf3ca136
Brendon Henrique <brendon.henrrique@hotmail.com>
2022-12-19 23:15:11 -0300

    exercicio01

---
+++ Exrcc_Fundamts_EstrtraDeContrle_Função/01.js
@@ -0,0 +1,19 @@
+function soma (x, y) {
+    return x + y
+}
+console.log(soma(4, 3))
+
+function subtração (x , y) {
+    return x - y
+}
+console.log(subtração(8, 5)) 
+
+function multiplicação (x, y) {
+    return x * y
+}
+console.log(multiplicação(5, 6))
+
+function divisão (x, y) {
+    return x / y
+}
+ console.log(divisão(40, 4))
